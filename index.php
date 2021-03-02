1feec7dda6be6f2ee009c2ababb7a34f5538dcf8
cj3636 <phillgilmartin@gmail.com>
2021-03-02 06:40:00 -0800

    Changed

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <!doctype html>
 <html lang="en_US">
-<?php require 'head.php'; ?>
-<body id="page" onload="getServerCookies()">
-<div class="ui fluid stackable grid">
-    <div class="center aligned sixteen wide column"></div>
-    <?php require 'form.php'; ?>
-    <footer>&copy; P.o.P Industries 2019</footer>
+<?php
+/* Main page with two forms: sign up and log in */
+require 'admin/db.php';
+require 'head.php';
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<body>
+<div id="page" class="ui stackable grid">
+    <div class="sixteen wide column"></div>
+    <div class="ui two wide column">
+        <img class="ui medium centered image" src="img/thenewjourney.png">
+    </div>
+    <div class="ui fourteen wide column">
+        <h1 class="ui title">Ligma | CS:GO Hack | Public Release</h1>
+        <h3 class="ui description">Discrete, Powerful, Affordable</h3>
+    </div>
+    <div class="sixteen wide column">
+        <?php require 'menu.php';?>
+    </div>
+</div>
+<footer>&copy; P.o.P Industries 2019</footer>
 </body>
 </html>
--- semantic/tasks/config/project/release.js
+++ semantic/tasks/config/project/release.js
@@ -24,7 +24,7 @@ try {
   npmPackage = require('../../../package.json');
 }
 catch(error) {
-  // generate fake package
+  // genChar fake package
   npmPackage = {
     name: 'Unknown',
     version: 'x.x'
